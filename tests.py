cca2d524667008a76df8ecd3be8900cb22ac1238
Javier Santacruz <javier.santacruz@avature.net>
2016-09-16 12:11:26 +0200

    Adds tests for sub-sub-repositories

--- tests.py
+++ tests.py
@@ -25,10 +25,14 @@ def create_repo(name):
     return path
 
 
+def submodule_update(path):
+    execute('git submodule update --init'.split(), cwd=path)
+
+
 def add_subrepo(master, origin, dest):
     origin = 'file://' + origin
     execute(['git', 'submodule', 'add', origin, dest], cwd=master)
-    execute('git submodule update --init'.split(), cwd=master)
+    submodule_update(master)
 
 
 def create_repository(name):
@@ -44,7 +48,7 @@ def create_repository(name):
     for subname, path in zip(subrepo_names, subrepos):
         add_subrepo(master_repo, path, os.path.join('subrepos', subname))
 
-    execute('git commit -am Subrepos'.split())
+    execute('git commit -am Submodules'.split(), cwd=master_repo)
 
     return master_repo, subrepos
 
@@ -78,32 +82,65 @@ class TestRun(object):
         assert_that(result, is_(True))
         assert_subrepos_relative(self.repo)
 
+    def test_list_subrepos_must_find_subrepos(self):
+        subrepos = list_submodules(find_git_dir(self.repo))
+
+        assert_that(subrepos, has_length(3))
+
+    def assert_submodule_config_is_fixed(self, path, worktree=None):
+        name = os.path.basename(path)
+        destination = worktree or os.path.join(self.repo, 'subrepos', name)
+        worktree_abs = os.path.abspath(os.path.join(path, get_worktree(path)))
+
+        assert_that(destination, is_(worktree_abs))
+
     def test_subrepos_worktree_points_to_its_place(self):
         relativize(self.repo)
 
-        for path in list_submodules(self.repo):
-            name = os.path.basename(path)
-            destination = os.path.join(self.repo, 'subrepos', name)
-            worktree_abs = os.path.abspath(path, get_worktree(path))
+        for path in list_submodules(find_git_dir(self.repo)):
+            self.assert_submodule_config_is_fixed(path)
 
-            assert_that(destination, is_(worktree_abs))
+    def assert_submodule_gitfile_is_fixed(self, path, worktree=None):
+        name = os.path.basename(path)
+        worktree = worktree or os.path.join(self.repo, 'subrepos', name)
+        with open(os.path.join(worktree, '.git')) as stream:
+            gitdir_rel = stream.read().split(':')[1].strip()
+        gitdir_abs = os.path.abspath(os.path.join(worktree, gitdir_rel))
+
+        assert_that(gitdir_abs, is_(path))
 
     def test_subrepos_gitdir_file_points_to_git_directory(self):
         relativize(self.repo)
 
-        for path in list_submodules(self.repo):
-            name = os.path.basename(path)
-            worktree = os.path.join(self.repo, 'subrepos', name)
-            with open(os.path.join(worktree, '.git')) as stream:
-                gitdir_rel = stream.read().split(':')[1].strip()
-            gitdir_abs = os.path.abspath(os.path.join(worktree, gitdir_rel))
+        for path in list_submodules(find_git_dir(self.repo)):
+            self.assert_submodule_gitfile_is_fixed(path)
 
-            assert_that(gitdir_abs, is_(path))
-
-    def test_subrepos_get_fixed_recursively(self):
+    def test_list_subrepos_must_find_subrepos_in_different_locations(self):
         # Create a repo with subrepos instead or master repo
         subrepo_path, _ = create_repository('submaster')
         add_subrepo(self.repo, subrepo_path, 'sub_master')
 
-        submodules = list_submodules(self.repo)
-        assert_that(submodules, has_length(4))
+        subrepos = list_submodules(find_git_dir(self.repo))
+
+        assert_that(subrepos, has_length(4))
+
+    def test_subrepos_get_fixed_recursively(self):
+        # Create a repo with subrepos instead or master repo
+        origin, _ = create_repository('submaster')
+        add_subrepo(self.repo, origin, 'sub_master')
+        subrepo_path = os.path.join(self.repo, '.git/modules', 'sub_master')
+
+        # Initialize subsubrepositories
+        subrepo_worktree = os.path.join(self.repo, 'sub_master')
+        submodule_update(subrepo_worktree)
+
+        relativize(self.repo)
+
+        subsubmodules = list_submodules(subrepo_path)
+        assert_that(subsubmodules, has_length(3))
+
+        for path in subsubmodules:
+            name = os.path.basename(path)
+            worktree = os.path.join(subrepo_worktree, 'subrepos', name)
+            self.assert_submodule_config_is_fixed(path, worktree)
+            self.assert_submodule_gitfile_is_fixed(path, worktree)
