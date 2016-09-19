da672fc8570a2b39186062882499e944ff639978
Javier Santacruz <javier.santacruz@avature.net>
2016-09-19 10:06:50 +0200

    Fixes brew formula

--- brew/git-relativize.rb
+++ brew/git-relativize.rb
@@ -1,11 +1,12 @@
 require "formula"
 
-class Git_Relativize < Formula
+class GitRelativize < Formula
   version "0.0.1"
   desc "Fix git subrepositories with absolute paths"
-  url "https://gitlab.xcade.net/avature/git-relativize.git", :using => :git
-  homepage "https://gitlab.xcade.net/avature/git-relativize"
-  depends_on :python, :git
+  url "https://gitlab.xcade.net/javier.santacruz/git-relativize.git", :using => :git
+  homepage "https://gitlab.xcade.net/javier.santacruz/git-relativize"
+  depends_on :python
+  depends_on "git"
 
   include Language::Python::Virtualenv
 
